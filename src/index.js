f82a7b111fb96b3ede1712b29fa8e289a83ab821
xiaoyei666 <xiaoyei666@sina.com>
2020-01-08 06:02:08 +0800

    v3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 import './style.css';
-
+import banner from './banner.jpg';
 import _ from 'lodash/lodash';
 
 function component() {
@@ -8,7 +8,12 @@ function component() {
 
   // lodash（目前通过一个 script 引入）对于执行这一行是必需的
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  //添加类样式
   element.classList.add('hello');
+  //加入图片
+  var img=new Image();
+  img.src=banner;
+  element.apppendChild(img);
   
   return element;
 }
