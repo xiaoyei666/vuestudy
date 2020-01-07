4acb418393b13c2a0a92b34316fc518e7adff817
xiaoyei666 <xiaoyei666@sina.com>
2020-01-07 23:20:41 +0800

    v2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+
+import './style.css';
+
 import _ from 'lodash/lodash';
 
 function component() {
@@ -5,7 +8,8 @@ function component() {
 
   // lodash（目前通过一个 script 引入）对于执行这一行是必需的
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
+  element.classList.add('hello');
+  
   return element;
 }
 
